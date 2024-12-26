e109cc6a334857268b7ad629d8d66a727536853a
Chris <christopher.doss92@gmail.com>
2024-12-26 12:27:47 -0600

    we're sliding now. would be nice now to be able to select the disk first, then move on the second click

--- scenes/red_disk.gd
+++ scenes/red_disk.gd
@@ -1,6 +1,6 @@
-extends Area2D
+extends CharacterBody2D
 
-var speed = 100
+var speed = 10
 var destination = position
 
 # Called when the node enters the scene tree for the first time.
@@ -13,5 +13,13 @@ func _process(delta: float) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		destination = get_global_mouse_position()
 	if position != destination:
-		position = destination
-	pass
+		# This changes position instantly. I'd like for it to slide into place
+		#position = destination
+		
+		# Attempt at adding sliding
+		var path: Vector2
+		path.x = destination.x - position.x
+		path.y = destination.y - position.y
+		
+		position.x += path.x * speed * delta
+		position.y += path.y * speed * delta
